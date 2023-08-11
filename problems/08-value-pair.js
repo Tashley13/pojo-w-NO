b72b3fa063b1c6cb530efc496631125866175b21
harmony <harmony.rosenberg94@gmail.com>
2023-08-10 19:36:20 -0500

    08

--- problems/08-value-pair.js
+++ problems/08-value-pair.js
@@ -1,7 +1,8 @@
 /***********************************************************************
-Write a function `valuePair(obj1, obj2, key)` that takes in two objects
-and a key (string). The function should return an array containing the
-corresponding values of the objects for the given key.
+Write a function `valuePair(obj1, obj2, key)`
+that takes in two objects snd a key (string).
+The function should return an array
+containing the corresponding values of the objects for the given key.
 
 Examples:
 let object1 = {name: 'One', location: 'NY', age: 3};
@@ -9,10 +10,16 @@ let object2 = {name: 'Two', location: 'SF'};
 valuePair(object1, object2, 'location'); // => [ 'NY', 'SF' ]
 valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 ***********************************************************************/
+/*
+declare our func
+declare variables using object values
+*/
 
 function valuePair(obj1, obj2, key) {
-  // Your code here
-}
+  const value1 = obj1[key]
+  const value2 = obj2[key]
+  return [value1, value2]
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuePair;
